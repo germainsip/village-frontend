88691801ce6eeb67a8bdd08791bfa222c296eb00
Germain SIPIERE <gsipiere@gmail.com>
2019-01-22 11:42:17 +0100

    ok

--- validation.js
+++ validation.js
@@ -7,58 +7,57 @@ var ville = document.getElementById("ville");
 var tel = document.getElementById("telephone");
 var mail = document.getElementById("mail");
 
+btn1.addEventListener("click", function() {
+  var cbon = true;
+  var regPre = /^\D{2,20}$/;
+  if (regPre.test(prenom.value) == false) {
+    cbon = false;
+    prenom.className = "invalid";
+  } else {
+    prenom.className = "valid";
+  }
+  var regNom = /^\D{2,20}$/;
+  if (regNom.test(nom.value) == false) {
+    cbon = false;
+    nom.className = "invalid";
+  } else {
+    nom.className = "valid";
+  }
+  var regeAdr = /^[0-9]{1,3}[a-z A-Z]{2,30}$/;
+  if (regeAdr.test(adresse.value) == false) {
+    cbon = false;
+    adresse.className = "invalid";
+  } else {
+    adresse.className = "valid";
+  }
+  var regeCP = /^[0-9]{5}$/;
+  if (regeCP.test(cp.value) == false) {
+    cbon = false;
+    cp.className = "invalid";
+  } else {
+    cp.className = "valid";
+  }
+  var regVille = /^\D{2,20}$/;
+  if (regVille.test(ville.value) == false) {
+    cbon = false;
+    ville.className = "invalid";
+  } else {
+    ville.className = "valid";
+  }
 
+  var regTel = /(\+\d+(\s|-))?0\d(\s|-)?(\d{2}(\s|-)?){4}/;
+  if (regTel.test(tel.value) == false) {
+    cbon = false;
+    tel.className = "invalid";
+  } else {
+    tel.className = "valid";
+  }
+  if (mail.className === "validate invalid") {
+    cbon = false;
+  }
+  console.log(cbon);
 
-btn1.addEventListener("click", function () {
-    var cbon = true;
-//    TODO: améliorer regex pour françois régis
-    var regPre =/^\D{2,20}$/;
-    if (regPre.test(prenom.value)==false) {
-        cbon = false;
-        prenom.className = "invalid";
-    } else {
-        prenom.className = "valid";
-      
-    }
-    var regNom = /^\D{2,20}$/;
-    if (regNom.test(nom.value)==false) {
-        cbon = false;
-        nom.className = "invalid";
-    } else {
-        nom.className = "valid";
-
-    }
-    var regeAdr = /^[0-9]{1,3}[a-z A-Z]{2,30}$/;
-    if (regeAdr.test(adresse.value)==false) {
-        cbon = false;
-        adresse.className = "invalid";
-    } else {
-        adresse.className = "valid";
-    }
-    var regeCP = /^[0-9]{5}$/;
-    if (regeCP.test(cp.value)==false) {
-        cbon = false;
-        cp.className = "invalid";
-    } else {
-        cp.className = "valid";
-    }
-// TODO: regex ville
-    var regeVille = /^\D{2,20}$/;
-    if (regeVille.test(cp.value)==false) {
-        cbon = false;
-        ville.className = "invalid";
-    } else {
-        ville.className = "valid";
-    }
-
-    var regTel = /(\+\d+(\s|-))?0\d(\s|-)?(\d{2}(\s|-)?){4}/
-    if (regTel.test(tel.value) == false){
-        cbon = false;
-        tel.className = "valid";
-    }
-    else {
-        tel.className = "valid";
-    }
-    
-    console.log(cbon);
+  if (cbon) {
+    document.getElementById("form1").submit();
+  }
 });
